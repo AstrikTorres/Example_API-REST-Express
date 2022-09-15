f4318d221c5773734473eccd4a0f44984c6ee46e
AstrikTorres <astriktorres@gmail.com>
2022-09-15 04:17:08 -0500

    GGET: parámetros query

--- index.js
+++ index.js
@@ -1,29 +1,53 @@
 const express = require('express');
+const faker = require('faker');
 const app = express();
 const port = 3000;
+
+products = [];
+categories = [];
+for (let i = 1; i < 100; i++) {
+  products.push({
+    id: i,
+    name: faker.commerce.productName(),
+    price: faker.commerce.price(),
+    image: faker.image.image(),
+    categoryId: faker.random.number({ min: 1, max: 10 }),
+  });
+}
+for (let i = 1; i < 10; i++) {
+  categories.push({
+    id: i,
+    name: faker.commerce.department(),
+    products: products.filter((p) => p.categoryId === i),
+  });
+}
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.get('/api/products', (req, res) => {
-  res.json({
-    name: 'Product 1',
-    price: 100
-  });
+app.get('/products', (req, res) => {
+  const { limit, offset } = req.query;
+  res.json(products.slice((offset || 0), (limit || 10)));
 });
 
-app.get('/api/products/:id', (req, res) => {
+app.get('/products/filter', (req, res) => {
+  res.json(products.filter((p) => p.name.includes(req.query.content)));
+});
+
+app.get('/products/:id', (req, res) => {
   const { id } = req.params;
 
-  res.json({
-    id,
-    name: `Product ${id}` ,
-    price: 100
-  });
+  const product = products.find((p) => p.id === parseInt(id));
+  if (product) {
+    res.json(product);
+  }
+  res.status(404).json({ message: 'Product not found' });
 });
 
-app.get('/api/categories/:categoryId/products/:productId', (req, res) => {
+
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
   const { categoryId, productId } = req.params;
 
   res.json({
@@ -32,6 +56,22 @@ app.get('/api/categories/:categoryId/products/:productId', (req, res) => {
     name: `Product ${productId}`,
     price: 100
   });
+});
+
+// Query Parameters
+app.get('/users', (req, res) => {
+  const { limit, offset } = req.query;
+
+  if (limit && offset) {
+    res.json({
+      limit,
+      offset
+    });
+  } else {
+    res.json({
+      message: 'No limit or offset'
+    });
+  }
 });
 
 app.listen(port, () => {
