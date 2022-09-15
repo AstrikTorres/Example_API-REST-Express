390e4148a846f6b9f20793fe9f02d8d1a812c831
AstrikTorres <astriktorres@gmail.com>
2022-09-15 03:53:21 -0500

    GET: recibir parámetros

--- index.js
+++ index.js
@@ -6,6 +6,34 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+app.get('/api/products', (req, res) => {
+  res.json({
+    name: 'Product 1',
+    price: 100
+  });
+});
+
+app.get('/api/products/:id', (req, res) => {
+  const { id } = req.params;
+
+  res.json({
+    id,
+    name: `Product ${id}` ,
+    price: 100
+  });
+});
+
+app.get('/api/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+
+  res.json({
+    categoryId,
+    productId,
+    name: `Product ${productId}`,
+    price: 100
+  });
+});
+
 app.listen(port, () => {
   console.log(`My port ${port}`);
 });
